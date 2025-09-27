93a83aa32b5362ac00549423bd4e7c4c5c511de3
Javier Solis <exmaple@example.com>
2025-09-27 06:40:58 -0500

    [ADD] Toucheable

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,14 @@
 import { useRouter } from "expo-router";
 import { useState } from "react";
-import { Button } from "react-native";
+import { Button, TouchableHighlight } from "react-native";
 import { styled, ThemeProvider } from "styled-components/native";
 import { Colors } from "../styles/themes";
 
 export default function Screen1() {
   const router = useRouter();
+  const [count, setCount] = useState(0);
+
+  const onPress = () => setCount((prev) => prev + 1);
 
   const [isDarkTheme, setIsDarkTheme] = useState(false);
   const toggleTheme = () => {
@@ -17,7 +20,16 @@ export default function Screen1() {
     <ThemeProvider theme={currentTheme}>
       <Container>
         <Title>Holaaaaa</Title>
-        <Button title="Ir a 2.11" onPress={() => router.push("/screen2")} />
+
+        <TouchableHighlight
+          underlayColor="#DDDEEE"
+          activeOpacity={0.6}
+          onPress={onPress}
+        >
+          <Title>Presioname :{count}</Title>
+        </TouchableHighlight>
+
+        <Button title="Ir a 2.12" onPress={() => router.push("/screen2")} />
         <Button title="Cambiar tema" onPress={toggleTheme} />
       </Container>
     </ThemeProvider>
