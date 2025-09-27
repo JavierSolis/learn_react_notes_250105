9cdbaa56b547ba4f20a8c2e73e395275273a98e8
Javier Solis <exmaple@example.com>
2025-09-27 06:51:33 -0500

    [ADD] Imagenes

--- app/index_backup1.tsx
+++ app/index_backup1.tsx
@@ -1,29 +1,40 @@
 import { useRouter } from "expo-router";
 import { useState } from "react";
+import { Button, TouchableHighlight } from "react-native";
 import { styled, ThemeProvider } from "styled-components/native";
 import { Colors } from "../styles/themes";
 
-import imagenLogo from "../assets/images/favicon.png";
-
-export default function Screen1() {
+export default function Screen1_Backup_1() {
   const router = useRouter();
+  const [count, setCount] = useState(0);
+
+  const onPress = () => setCount((prev) => prev + 1);
+
   const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const toggleTheme = () => {
+    setIsDarkTheme((prevTheme) => !prevTheme);
+  };
   const currentTheme = isDarkTheme ? Colors.dark1 : Colors.light1;
 
   return (
     <ThemeProvider theme={currentTheme}>
       <Container>
-        <Imagen source={require("../assets/images/icon.png")} />
-        <Imagen source={imagenLogo} />
+        <Title>Holaaaaa</Title>
+
+        <TouchableHighlight
+          underlayColor="#DDDEEE"
+          activeOpacity={0.6}
+          onPress={onPress}
+        >
+          <Title>Presioname :{count}</Title>
+        </TouchableHighlight>
+
+        <Button title="Ir a 2.12" onPress={() => router.push("/screen2")} />
+        <Button title="Cambiar tema" onPress={toggleTheme} />
       </Container>
     </ThemeProvider>
   );
 }
-
-const Imagen = styled.Image`
-  width: 100px;
-  height: 100px;
-`;
 
 const Title = styled.Text`
   color: #151313ff;
