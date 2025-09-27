907073bfaf8c999f135ee1e414d65e01f483ed27
Javier Solis <exmaple@example.com>
2025-09-27 06:59:15 -0500

    [ADD] Scrtollview and textinput

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "expo-router";
 import { useState } from "react";
+import { ScrollView } from "react-native";
 import { styled, ThemeProvider } from "styled-components/native";
 import { Colors } from "../styles/themes";
 
@@ -12,13 +13,30 @@ export default function Screen1() {
 
   return (
     <ThemeProvider theme={currentTheme}>
-      <Container>
-        <Imagen source={require("../assets/images/icon.png")} />
-        <Imagen source={imagenLogo} />
-      </Container>
+      <ScrollView>
+        <Container>
+          <Imagen source={require("../assets/images/icon.png")} />
+          <Imagen source={imagenLogo} />
+          <TextEntrada
+            keyboardType="numeric"
+            placeholder="Ingrese su numero aquí"
+          ></TextEntrada>
+        </Container>
+      </ScrollView>
     </ThemeProvider>
   );
 }
+
+const TextEntrada = styled.TextInput`
+  height: 40px;
+  border-color: gray;
+  border-width: 1px;
+  width: 80%;
+  margin-top: 20px;
+  padding: 10px;
+  color: #151313ff;
+  background-color: #ffffff;
+`;
 
 const Imagen = styled.Image`
   width: 100px;
