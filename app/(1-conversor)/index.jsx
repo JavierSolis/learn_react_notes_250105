bddccf1bf2df13f9486f9a74eddc53a4b1ef3c53
Javier Solis <exmaple@example.com>
2025-09-28 07:13:24 -0500

    [ADD] Imagen y plantulla base de componente

--- app/(1-conversor)/index.jsx
+++ app/(1-conversor)/index.jsx
@@ -3,7 +3,15 @@ import styled from "styled-components/native";
 export default function Componente() {
   return (
     <Contenedor>
-      <Texto>Componente</Texto>
+      <Icon
+        source={{ uri: "https://i.ibb.co/xTXWP76/modista.png" }}
+        resizeMode="contain"
+      />
+      <Title>Conversor</Title>
+      <Subtitle>(cm) a (m)</Subtitle>
+      <StartButton>
+        <ButtonText>Iniciar</ButtonText>
+      </StartButton>
     </Contenedor>
   );
 }
@@ -13,6 +21,26 @@ const Contenedor = styled.View`
   justify-content: center;
   align-items: center;
 `;
-const Texto = styled.Text`
+const Title = styled.Text`
   font-size: 20px;
+`;
+const Subtitle = styled.Text`
+  font-size: 10px;
+`;
+
+const StartButton = styled.TouchableOpacity`
+  background-color: #54c558ff;
+  padding: 10px 20px;
+  border-radius: 5px;
+  margin-top: 20px;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+  font-size: 16px;
+`;
+
+const Icon = styled.Image`
+  width: 204px;
+  height: 204px;
 `;
