7e98afbfb334cc99f7656b2745ae740382aecf24
Javier Solis <exmaple@example.com>
2025-09-29 05:42:14 -0500

    [ADD] Uso de HREF, Stylos de componente no nativos

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,8 @@ import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
       {/*<Stack.Screen name="(1-conversor)" options={{ headerShown: false }} />*/}
       {/*<Stack.Screen name="(2-listas)" options={{ headerShown: false }} />*/}
     </Stack>
--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,20 @@
+import { Link } from "expo-router";
 import { FlatList } from "react-native";
 import styled from "styled-components/native";
 
 export default function Componente() {
   const dataRutas = [
-    { key: 1, name: "(1-conversor)" },
-    { key: 2, name: "(2-listas)" },
+    { key: 1, name: "(1-conversor)", href: "/(1-conversor)" },
+    { key: 2, name: "(2-listas)", href: "/(2-listas)" },
   ];
 
-  const renderItem = ({ item }) => <Title>Título: {item.name}</Title>;
+  const renderItem = ({ item }) => {
+    return (
+      <LinkButton href={item.href}>
+        <Title>Título: {item.name}</Title>
+      </LinkButton>
+    );
+  };
 
   return (
     <Contenedor>
@@ -31,4 +38,13 @@ const Texto = styled.Text`
 `;
 const Title = styled.Text`
   font-size: 20px;
+`;
+
+const LinkButton = styled(Link)`
+  padding: 15px;
+  margin-bottom: 10px;
+  border-radius: 20px;
+  flex-direction: row;
+  border: 2px solid #cecece;
+  width: 100%;
 `;
